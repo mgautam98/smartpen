e2851c72f9edfbe7d76a9c51a7d708b55651fb65
Gautam Mishra <mishra.gautam96@gmail.com>
2020-03-26 04:27:31 +0530

    img display

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for
+from flask import Flask, render_template, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -13,10 +13,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-# @app.route('/')
-# def home():
-#     return render_template('home.html')
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
@@ -34,23 +30,18 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-from flask import send_from_directory
+            print(filename)
+            return redirect(url_for('display', filename=filename))
+    return render_template('home.html')
 
-@app.route('/uploads/<filename>')
+@app.route('/upload/<filename>')
 def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    return send_from_directory("uploads", filename)
+
+
+@app.route('/display/<filename>')
+def display(filename):
+    return render_template("display.html", image_name=filename)
                                
 if __name__ == '__main__':
     app.run()
